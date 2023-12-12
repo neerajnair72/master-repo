2760d0f8317a4e746d34592a3eb980810a815e7c
sudhirj13 <78610201+sudhirj13@users.noreply.github.com>
2023-12-12 19:15:06 +0530

    auto-heal upload

---
+++ src/test/java/AutoHeal.java
@@ -0,0 +1,90 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class AutoHeal
+{
+    RemoteWebDriver driver = null;
+    public static String status = "passed";
+    public static String username = System.getenv("LT_USERNAME");
+    public static String access_key = System.getenv("LT_ACCESS_KEY");
+
+
+    @BeforeMethod
+    @Parameters(value={"browser","version","platform", "resolution"})
+    public void testSetUp(String browser, String version, String platform, String resolution) throws Exception
+    {
+        String platformName = System.getenv("HYPEREXECUTE_PLATFORM") != null ? System.getenv("HYPEREXECUTE_PLATFORM") : platform;
+        
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("build", "[HyperExecute - 1] Demonstration of the AutoHeal");
+        capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("version", version);
+
+        capabilities.setCapability("tunnel",false);
+        capabilities.setCapability("network",true);
+        capabilities.setCapability("console",true);
+        capabilities.setCapability("visual",true);
+        capabilities.setCapability("autoHeal", true);
+
+        try
+        {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + access_key + "@hub.lambdatest.com/wd/hub"), capabilities);
+        }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Invalid grid URL");
+        }
+        System.out.println("Started session");
+    }
+
+    @Test()
+    public void autoheal() throws InterruptedException
+    {
+
+        try {
+
+            driver.get("https://www.lambdatest.com/selenium-playground/auto-healing");
+            Thread.sleep(5000);
+    
+            WebElement changedom = driver.findElementByXPath("//*[contains(text(), 'Change DOM ID')]");
+            // changedom.click();      //Uncomment this line in the 2nd test run for the autoheal to work. 
+    
+            WebElement username = driver.findElementById("username");
+            username.sendKeys("test@gmail.com");
+        
+            WebElement password = driver.findElementById("password");
+            password.sendKeys("password");
+    
+            WebElement login = driver.findElementByXPath("//*[contains(text(), 'Submit')]");
+            login.click();
+
+        } catch (Exception e) {
+            status = "failed";
+        }
+
+
+
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        if (driver != null)
+        {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+            driver.quit();
+        }
+    }
+}
