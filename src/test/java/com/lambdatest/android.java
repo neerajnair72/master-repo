883adfb290d53141c0bf3066b6f57d20ad7f3a52
neerajnair72 <69789124+neerajnair72@users.noreply.github.com>
2024-04-18 13:48:40 +0530

    Update android.java

--- src/test/java/com/lambdatest/android.java
+++ src/test/java/com/lambdatest/android.java
@@ -33,7 +33,7 @@ public class android {
         capabilities.setCapability("isRealMobile", true);
         //capabilities.setCapability("dedicatedProxy", true);
         // capabilities.setCapability("platformVersion","11");
-        capabilities.setCapability("app","lt://APP1016035381697113789364258"); //Enter the App ID here
+        capabilities.setCapability("app","lt://APP10160492141713425352287504"); //Enter the App ID here
         capabilities.setCapability("deviceOrientation", "portrait");
         capabilities.setCapability("console",true);
         capabilities.setCapability("network",true);
